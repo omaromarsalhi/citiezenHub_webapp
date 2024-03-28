cfbd9eaebd3aded7f21d2dcee61191b8899b4b2e
aziz gmaty <mohamedaziz.gmaty@esprit.tn>
2024-03-28 14:29:59 +0100

    28/03/2024

--- src/Controller/TransportController.php
+++ src/Controller/TransportController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Transport;
+use App\Form\TransportType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TransportController extends AbstractController
 {
-    #[Route('/addAbonnement', name: 'app_transport')]
-    public function index(): Response
+
+
+    #[Route('/showAbonnement', name: 'app_transport')]
+    public function afficherAbn(): Response
     {
-        return $this->render('transport/createAbonnement.html.twig', [
-            'controller_name' => 'TransportController',
+        $transport=$this->getDoctrine()->getManager()->getRepository(Transport::class)->findAll();
+        return $this->render('transport/showAbonnement.html.twig', [
+            'l' => $transport
         ]);
+    }
+
+    #[Route('addTransport', name: 'addTransport')]
+
+    public function addTransport(Request $request): Response
+    {
+        $transport = new Transport();
+        $form = $this->createForm(TransportType::class, $transport);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($transport);
+            $em->flush();
+            return $this->redirectToRoute('app_transport');
+        }
+        else{
+            return $this->render('transport/createAbonnement.html.twig',['f' => $form->createView()]);
+        }
+
+
     }
 
 }
