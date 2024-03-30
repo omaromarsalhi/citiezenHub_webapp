fa78876a884e65611e7ab218fe906653f17b090b
latifa benzaied <latifa.benzaied@esprit.tn>
2024-03-30 18:15:28 +0100

    lllll

--- src/Utils/GoogleAuthentificator.php
+++ src/Utils/GoogleAuthentificator.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class GoogleAuthentificator extends SocialAuthenticator
-{//lllll
+{  //lllll
 
     private $clientRegistry;
     private $entityManager;
