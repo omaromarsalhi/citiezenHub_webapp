90cc86f9fff2a3c94a514916aeaac178bad1cf18
khalil <khalil.rmila@esprit.tn>
2024-03-31 01:29:41 +0100

    Update ReclamationshController and index template to display reclamation data
    The changes include:
    - Renaming the route name and path of the index action in ReclamationshController
    - Injecting EntityManagerInterface in the index action to fetch all reclamations using the Reeclamation repository
    - Passing the fetched reclamations to the index template as a variable
    - Updating the index template to display the reclamations data using a for loop
    - Adding HTML and CSS code to style the reclamations data
    - Including a link to the reclamation details page for each reclamation
    
    Additionally, the diff also includes changes to the reclamation/index template to update the title and body content. However, these changes are not directly related to the reclamationsh feature.

--- src/Controller/ReclamationshController.php
+++ src/Controller/ReclamationshController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Reeclamation;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReclamationshController extends AbstractController
 {
-    #[Route('/reclamationsh', name: 'app_reclamationsh')]
-    public function index(): Response
+    #[Route('/', name: 'app_reclamationsh')]
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $rec=$entityManager->getRepository(Reeclamation::class)->findAll();
         return $this->render('reclamationsh/index.html.twig', [
             'controller_name' => 'ReclamationshController',
+            'rec' => $rec
         ]);
     }
 }
