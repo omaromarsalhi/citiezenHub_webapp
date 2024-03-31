b30c3c398ec48ad82652534b707bb9bfee1b30b9
khalil <khalil.rmila@esprit.tn>
2024-04-01 00:24:00 +0100

    Update ReclamationshController and index.html.twig: - Change findAll() to findBy() with 'DESC' order by createdAt - Extract last reclamations for each subject - Limit the number of reclamations displayed in the index view to 4

--- src/Controller/ReclamationshController.php
+++ src/Controller/ReclamationshController.php
@@ -13,11 +13,20 @@ class ReclamationshController extends AbstractController
     #[Route('/', name: 'app_reclamationsh')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $rec=$entityManager->getRepository(Reeclamation::class)->findAll();
+        $recc = $entityManager->getRepository(Reeclamation::class)->findBy([], ['createdAt' => 'DESC']);
+        $last_reclamations = [];
+
+        foreach ($recc as $reclamation) {
+            $subject = $reclamation->getSubject();
+
+            if (!isset($rec[$subject]) || $rec[$subject]->getCreatedAt() < $reclamation->getCreatedAt()) {
+                $rec[$subject] = $reclamation;
+            }
+        }
+
         return $this->render('reclamationsh/index.html.twig', [
             'controller_name' => 'ReclamationshController',
             'rec' => $rec
         ]);
     }
-    
 }
